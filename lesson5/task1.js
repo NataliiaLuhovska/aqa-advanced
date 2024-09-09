80b7ba571b7650780fa95bb4fe48913d07817b2a
nataliia luhovska <nluhovska@cordial.com>
2024-09-09 15:11:08 +0200

    home task 5

---
+++ lesson5/task1.js
@@ -0,0 +1,15 @@
+let averageGrade = 85;
+
+if (averageGrade < 60) {
+  console.log("Unsatisfactory");
+} else if (averageGrade >= 60 && averageGrade <= 70) {
+  console.log("Satisfactory");
+} else if (averageGrade > 70 && averageGrade <= 80) {
+  console.log("Good");
+} else if (averageGrade > 80 && averageGrade <= 90) {
+  console.log("Very good");
+} else if (averageGrade > 90 && averageGrade <= 100) {
+  console.log("Excellent");
+} else {
+  console.log("This grade is not allowed");
+}
---
+++ lesson5/task3_1.js
@@ -0,0 +1,5 @@
+let multTable = 8;
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`${multTable} * ${i} = ${multTable * i}`);
+}
